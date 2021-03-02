f19162c738a3495ffdaaba129fec2458ebc4cf4d
Dev Singh <dev@devksingh.com>
2021-03-02 23:01:58 +0000

    push bot config

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: process.env.DISCORD_TOKEN,
+        prefix: process.env.PREFIX,
+        activity: process.env.ACTIVITY,
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
