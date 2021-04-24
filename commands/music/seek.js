27e01f9f6f60976136e3105a3713cb8a3f33b8ee
Dev Singh <dev@devksingh.com>
2021-04-24 17:56:19 +0000

    fix seeking functions

--- commands/music/seek.js
+++ commands/music/seek.js
@@ -6,13 +6,13 @@ module.exports = {
 
     execute(client, message, args) {
         if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel!`);
-
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - You are not in the same voice channel!`);
 
         if (!args[0]) return message.channel.send(`${client.emotes.error} - Please indicate the position to seek to!`);
 
         try {
-            return client.player.play(message, args[0] * 1000);
+            client.player.seek(message, args[0] * 1000);
+            return message.channel.send(`${client.emotes.success} - Seeked to ${args[0]} seconds!`);
         } catch {
             return message.channel.send(`${client.emotes.error} - Please indicate a valid position to seek to!`);
         }
