d82732a9ed3a0338aea605268f2ed2149a045366
Dev Singh <dev@devksingh.com>
2023-07-11 21:50:12 -0500

    add logging statements

--- player/error.js
+++ player/error.js
@@ -7,15 +7,18 @@ module.exports = (client, error, message, ...args) => {
             message.channel.send(`${client.emotes.error} - You are not connected in any voice channel!`);
             break;
         case 'UnableToJoin':
+            console.log("UnableToJoin Error: " + str(e));
             message.channel.send(`${client.emotes.error} - I am not able to join your voice channel, please check my permissions!`);
             break;
         case 'VideoUnavailable':
+            console.log("VideoUnavailable Error: " + str(e));
             message.channel.send(`${client.emotes.error} - ${args[0].title} is not available in your country! Skipping...`);
             break;
         case 'MusicStarting':
             message.channel.send(`The music is starting... please wait and retry!`);
             break;
         default:
+            console.log("Error: " + str(e));
             message.channel.send(`${client.emotes.error} - Something went wrong ... Error : ${error}`);
     };
 };
