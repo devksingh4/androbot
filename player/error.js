e09f9e1444ee458a70aa6e824233c16351ca00c4
Dev Singh <dev@devksingh.com>
2023-07-11 21:54:53 -0500

    this is not python

--- player/error.js
+++ player/error.js
@@ -7,18 +7,18 @@ module.exports = (client, error, message, ...args) => {
             message.channel.send(`${client.emotes.error} - You are not connected in any voice channel!`);
             break;
         case 'UnableToJoin':
-            console.log("UnableToJoin Error: " + str(e));
+            console.log("UnableToJoin Error: " + error.toString());
             message.channel.send(`${client.emotes.error} - I am not able to join your voice channel, please check my permissions!`);
             break;
         case 'VideoUnavailable':
-            console.log("VideoUnavailable Error: " + str(e));
+            console.log("VideoUnavailable Error: " + error.toString());
             message.channel.send(`${client.emotes.error} - ${args[0].title} is not available in your country! Skipping...`);
             break;
         case 'MusicStarting':
             message.channel.send(`The music is starting... please wait and retry!`);
             break;
         default:
-            console.log("Error: " + str(e));
+            console.log("Error: " + error.toString());
             message.channel.send(`${client.emotes.error} - Something went wrong ... Error : ${error}`);
     };
 };
