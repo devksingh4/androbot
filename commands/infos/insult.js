11b9eaf33a5c31cf864f9028059efd21b315e84f
Dev Singh <dev@devksingh.com>
2023-07-18 14:05:01 -0500

    update message

--- commands/infos/insult.js
+++ commands/infos/insult.js
@@ -5,6 +5,6 @@ module.exports = {
     utilisation: '{prefix}insult <user>',
 
     execute(client, message, args) {
-        message.channel.send(`${args[0]} why are you so worthless? You bring dishonor to your whole family! Your parents did not work this hard to have a child as worthless as YOU!`);
+        message.channel.send(`${args[0]} why are you so worthless? You bring dishonor to your whole family! Your parents did not work this hard to have a child as WORTHLESS as you!`);
     },
 };
